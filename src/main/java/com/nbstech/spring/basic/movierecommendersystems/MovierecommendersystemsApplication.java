7a71c2360cb8a83e08195f8cce817f03249b89ac
MinhHue <minhhue14072002@gmail.com>
2022-12-06 06:32:46 +0700

    add another files

---
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystems/MovierecommendersystemsApplication.java
@@ -0,0 +1,13 @@
+package com.nbstech.spring.basic.movierecommendersystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecommendersystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovierecommendersystemsApplication.class, args);
+    }
+
+}
