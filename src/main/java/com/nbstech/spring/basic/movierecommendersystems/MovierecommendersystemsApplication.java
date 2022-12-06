802c0816b1d6d6fee126ed9a700f76dccd050b4c
MinhHue <minhhue14072002@gmail.com>
2022-12-06 07:24:37 +0700

    add movie recommender and contentbasedfilter

--- src/main/java/com/nbstech/spring/basic/movierecommendersystems/MovierecommendersystemsApplication.java
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystems/MovierecommendersystemsApplication.java
@@ -1,13 +1,26 @@
 package com.nbstech.spring.basic.movierecommendersystems;
 
+import com.nbstech.spring.basic.movierecommendersystems.Lesson1.MovieRecommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MovierecommendersystemsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MovierecommendersystemsApplication.class, args);
+        //SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+        //create object of RecommenderImplementation class
+        MovieRecommender recommender = new MovieRecommender();
+
+        //call method to get recommendations
+        String[] result = recommender.recommendMovies("Finding Dory");
+
+        //display results
+        System.out.println(Arrays.toString(result));
     }
 
 }
