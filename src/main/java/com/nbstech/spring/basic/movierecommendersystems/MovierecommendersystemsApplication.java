f56430813d9bdf97e2122202cd7cd78061ce6354
MinhHue <minhhue14072002@gmail.com>
2022-12-14 06:52:52 +0700

    Config Spring app manually without Spring Boot

--- src/main/java/com/nbstech/spring/basic/movierecommendersystems/MovierecommendersystemsApplication.java
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystems/MovierecommendersystemsApplication.java
@@ -3,25 +3,24 @@ package com.nbstech.spring.basic.movierecommendersystems;
 
 //import com.nbstech.spring.basic.movierecommendersystems.Lesson1.MovieRecommender;
 //import com.nbstech.spring.basic.movierecommendersystems.Lesson1.ContentBasedFilter;
-import com.nbstech.spring.basic.movierecommendersystems.Lesson2.CollaborativeFilter;
-import com.nbstech.spring.basic.movierecommendersystems.Lesson2.ContentBasedFilter;
-import org.springframework.boot.SpringApplication;
-import com.nbstech.spring.basic.movierecommendersystems.Lesson3.MovieRecommender;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import com.nbstech.spring.basic.movierecommendersystems.Lesson3.MovieRecommender;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
+@ComponentScan
 
-
-@SpringBootApplication
+//@SpringBootApplication
 public class MovierecommendersystemsApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(MovierecommendersystemsApplication.class, args);
         //SpringApplication.run(MovieRecommenderSystemApplication.class, args);
-        ApplicationContext appContext = SpringApplication.run(MovierecommendersystemsApplication.class,args);
+//        ApplicationContext appContext = SpringApplication.run(MovierecommendersystemsApplication.class,args);
+        AnnotationConfigApplicationContext appContext =
+                new AnnotationConfigApplicationContext(MovierecommendersystemsApplication.class);
         //create object of RecommenderImplementation class
 //        MovieRecommender recommender = new MovieRecommender(new ContentBasedFilter());
         MovieRecommender recommender =appContext.getBean(MovieRecommender.class);
